390c0b49bdcf193c06ac0d285ada703dcf93d777
jimmy <wjdtjq6@icloud.com>
2025-02-21 07:58:52 +0900

    Initial Commit

---
+++ CleanArchitectureMVVM/CleanArchitectureMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureMVVMApp.swift
+//  CleanArchitectureMVVM
+//
+//  Created by 소정섭 on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
